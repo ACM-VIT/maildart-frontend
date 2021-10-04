9258c6321d1b992ae275b7dc935a1419619b84a7
Ayush Goswami <ayushgoswami.abr@gmail.com>
2021-10-04 23:15:39 +0530

    Done Styling according to notion guide

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import Radio from './Components/Radio'
 function App() {
   return (
-    <>
     <div className="App">
-      <p class="bg-red-500">Something is here</p>
-    </div>
     <Radio/>
-    </>
+    </div>
   );
 }
 
--- src/Components/Radio.js
+++ src/Components/Radio.js
@@ -3,30 +3,30 @@ import React,{useState} from 'react'
 const Radio = () => {
     const [service, setservice] = useState('')
     return (
-        <div className="bg-red-500 flex-row p-10">
-      <h3>Please Choose Mailing Service</h3>
-      <form>
-          <ul>
-            <li>
-              <input type="radio" value="Listmonk" id="Listmonk" name="mail" onChange={()=>{setservice('Listmonk')}} checked={service==='Listmonk'}/>
+  <div className="bg-black text-white flex p-1">
+    <div className="flex-1">
+      <form >
+        <span className="p-4">
+              <input className="form-radio text-indigo-600" type="radio" value="Listmonk" id="Listmonk" name="mail" onChange={()=>{setservice('Listmonk')}} checked={service==='Listmonk'}/>
               <label for="listmonk">Listmonk</label>
-            </li>
-            <li>
+        </span>
+        <span className="p-4">
               <input type="radio" value="Nodemailer" id="Nodemailer" name="mail" onChange={()=>{setservice('Nodemailer')}} checked={service==='Nodemailer'}/>
               <label for="Nodemailer">Nodemailer</label>
-            </li>
-            <li>
+        </span>
+        <span className="p-4">
               <input type="radio" value="Mailgun" id="Mailgun" name="mail" onChange={()=>{setservice('Mailgun')}} checked={service==='Mailgun'}/>
               <label for="Mailgun">Mailgun</label>
-            </li>
-            <li>
+        </span>
+        <span className="p-4">
               <input type="radio" value="SendGrid" id="SendGrid" name="mail" onChange={()=>{setservice('SendGrid')}} checked={service==='SendGrid'}/>
               <label for="SendGrid">SendGrid</label>
-            </li>
-            </ul>
+        </span>
       </form>
-      {service && <h3>You have Chosen {service}</h3>}
-      {service && <button className="bg-white p-2 m-2 border-1 rounded hover:bg-gray-300" onClick={()=>{setservice('')}}>Clear Selection</button>}
+      </div>
+      <div className="flex-1">
+            <h1 className="flex-1 text-lg">PREVIEW</h1>
+      </div>
     </div>
     )
 }
