5489c057574d7683d2a1f2c05d3c93b18ba58699
satyamsingh07 <81974869+satyamsingh07@users.noreply.github.com>
2021-10-07 01:13:17 +0530

    Update TextArea.js
    changes made

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -2,15 +2,21 @@
 import React, { useState } from 'react';
 
 export default function TextArea() {
-    const [text, onChange] = useState(' ');
+    const [text, setText] = useState(' ');
     return (
-        <div >
+        <div className='h-full'>
             <h1>Email Template</h1>
-            <textarea className=' m-2
+            <textarea
+
+                type={'text'}
+                value={text}
+                placeholder={' '}
+                onChange={event => {
+                    setText(event.target.value)
+                }} className=' m-2 w-2/4 h-72
         border
         p-0.5'>
-                <input onChange={onChange}
-                    value={text}></input>
+
             </textarea>
 
 
@@ -25,4 +31,3 @@ export default function TextArea() {
 
 
 }
-
