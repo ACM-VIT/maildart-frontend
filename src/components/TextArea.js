29852d9a0c8c011f71a05e27412f7834ffeef11a
Satyam Singh <satyamsingh20027@gmail.com>
2021-10-06 16:09:08 +0530

    Update TextArea.js
    changes made

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -2,13 +2,13 @@
 import React, { useState } from 'react';
 
 export default function TextArea() {
-    const [text, onChange] = useState('enter here');
+    const [text, onChange] = useState(' ');
     return (
         <div >
             <h1>Email Template</h1>
-            <textarea className='justify-center m-2
+            <textarea className=' m-2
         border
-        padding: 10'>
+        p-0.5'>
                 <input onChange={onChange}
                     value={text}></input>
             </textarea>
