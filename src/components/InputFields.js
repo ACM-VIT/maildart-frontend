4c8ed55323850cfdf2ac973d0d2a5f33f7fa7ddc
Satyam Singh <satyamsingh20027@gmail.com>
2021-10-07 16:27:29 +0530

    Update InputFields.js
    changes made

--- src/components/InputFields.js
+++ src/components/InputFields.js
@@ -2,38 +2,48 @@
 import React, { useState } from 'react';
 
 
-const Input = ({ title }) => {
+const Input = ({ title, type }) => {
     const [text, setText] = useState(' ');
     return (
-        <div className='h-full'>
-            <h1>{title}</h1>
+        <div className='h-full py-4'  >
+            <h6 className='mx-2 py-0'>{title}</h6>
             <input
 
-                type={'text'}
+                type={type}
                 value={text}
+
+
+
                 placeholder={' '}
                 onChange={event => {
                     setText(event.target.value)
-                }} className=' m-2 w-2/4 
-        border
+                }} className='mx-2  w-2/4 
+            border
         p-0.5'>
 
             </input>
-
-
-
-
-
-
-        </div>)
+        </div >)
 }
 
+
 export default function InputFields() {
+
     return (
-        <div>
-            <Input title='Subject' />
-            <Input title='Recipients File' />
-            <Input title='Authkey/Credentials' />
+        <div className='mx-5 my-5'>
+            <Input title='Subject' type='text' />
+            <h6 className='mx-2'>Recipients File</h6>
+            <input className=' mx-2 w-2/4 
+            border
+       p-0.5' type="file"
+                onChange={(event) => {
+                    this.readFile(event)
+                }}
+                onClick={(event) => {
+                    event.target.value = null
+                }}
+
+            />
+            <Input title='Authkey/Credentials' type='text' />
         </div>)
 }
 
